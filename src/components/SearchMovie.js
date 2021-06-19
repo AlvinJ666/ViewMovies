cb7408b283984a9bd983e512222040865ea189ce
Alvin666 <jian0088@algonquinlive.com>
2021-06-19 09:24:42 -0400

    Add title, search input box, submit button, as well as some animations for better displaying

---
+++ src/components/SearchMovie.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const SearchMovie = () => {
+    return (
+        <>
+        <div id='content' className='section'>
+            <form className='form-control'>
+                <label className='form-label'>MOVIE NAME</label>
+                <input className='form-input' type="text" name='query' required/>
+
+                <button className='form-button' type='submit'>Search</button>
+            </form>
+        </div>
+        </>
+    )
+}
+
+export default SearchMovie;
